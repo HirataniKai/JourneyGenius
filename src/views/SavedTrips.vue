96f462fd725b9ff05bda2fe9b0eb05ec370bd5e7
Kai Hiratani <89423301+HirataniKai@users.noreply.github.com>
2023-12-01 13:54:44 -0800

    Skeleton Saved Trips
    Created the UI for the Saved Trips page. Implemented a grid for one saved vacation; San Francisco. In the future, the button will link up to that specific saved trip. For now, we will locally store information for prototype reasons.

--- src/views/SavedTrips.vue
+++ src/views/SavedTrips.vue
@@ -1,53 +1,88 @@
 <template>
-  <div class="home">
-    <h1 class="subtitle-1 grey--text">Saved Trips</h1>
-    <p>Two claps I rock</p>
+  <v-container>
+    <!-- Saved Trips Introduction -->
+    <v-row justify="center" class="mt-4">
+      <v-col cols="12" md="8" class="text-center">
+        <h1 class="headline text-deep-purple-accent-2">Saved Trips</h1>
+        <p>
+          Explore and manage your saved adventures effortlessly on our "Saved Trips" page. Easily access
+          and review your curated itineraries, and if inspiration strikes or plans change, customize them
+          to suit your preferences. This hub is your go-to destination for comprehensive details about
+          your planned journeys, allowing you to delve into more information and make adjustments seamlessly.
+          Your personalized travel experiences are just a click away on the Saved Trips page.
+        </p>
+        <br>
+        <hr>
+      </v-col>
+    </v-row>
 
-    <v-container class="my-5">
-      <v-layout row wrap>
-        <v-col xs="12" md="6">
-          <v-btn outlined block class="primary">1</v-btn>
-        </v-col>
-        <v-col xs="12" md="6">
-          <v-btn outlined block class="primary">2</v-btn>
-        </v-col>
-      </v-layout>
+    <!-- Saved Trip Card -->
+    <v-row justify="center">
+      <v-col cols="12" md="8">
+        <v-card class="pa-4 mb-4">
+          <!-- Title and Delete Button Section -->
+          <v-row align="center">
+            <v-col cols="8">
+              <h2 class="headline">San Francisco, California</h2>
+            </v-col>
+            <v-col cols="4" class="text-right">
+              <v-btn icon>
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
 
-      <v-layout row wrap>
-        <v-col xs="12" md="6">
-          <v-btn outlined block class="primary">1</v-btn>
-        </v-col>
-        <v-col xs="4" md="2">
-          <v-btn outlined block class="primary">2</v-btn>
-        </v-col>
-        <v-col xs="4" md="2">
-          <v-btn outlined block class="primary">3</v-btn>
-        </v-col>
-        <v-col xs="4" md="2">
-          <v-btn outlined block class="primary">4</v-btn>
-        </v-col>
-      </v-layout>
+          <!-- Main Content Section -->
+          <v-row>
+            <!-- Description and Activities Section -->
+            <v-col cols="12" md="6">
+              <p>
+                San Francisco is a vibrant city known for its iconic landmarks, diverse culture, and stunning views.
+                Explore the Golden Gate Bridge, visit Alcatraz Island, and stroll through Fisherman's Wharf for a
+                taste of the city's rich history and delicious seafood.
+              </p>
+              <br>
+              <h3 class="subtitle-1">Activities:</h3>
+              <ul>
+                <li>Golden Gate Bridge Sightseeing</li>
+                <li>Alcatraz Island Tour</li>
+                <li>Fisherman's Wharf Exploration</li>
+                <!-- Add more activities as needed -->
+              </ul>
+            </v-col>
 
-      <v-layout row wrap>
-        <v-flex xs="4" md="3">
-          <v-btn outlined block class="success">yay</v-btn>
-        </v-flex>
-      </v-layout>
-
-
-
-    </v-container>
-
-
-  </div>
+             <!-- Image and Open Itinerary Button Section -->
+      <v-col cols="12" md="6">
+        <v-row align="center" justify="center">
+          <v-img
+            src="@/assets/sf.jpeg"
+            alt="San Francisco"
+            class="mb-3"
+            style="width: 100%; border-radius: 8px;"
+          ></v-img>
+          <v-btn color="primary" class="mt-3" @click="openItinerary">
+            Open Itinerary
+          </v-btn>
+        </v-row>
+      </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
-  
+
 <script>
-import { defineComponent } from 'vue';
-
-
-export default defineComponent({
-
-});
+export default {
+  methods: {
+    openItinerary() {
+      // Implement logic to open the itinerary for this saved trip
+      console.log('Opening Itinerary...');
+    },
+  },
+};
 </script>
-  
+
+<style scoped>
+/* Add custom styles if needed */
+</style>
